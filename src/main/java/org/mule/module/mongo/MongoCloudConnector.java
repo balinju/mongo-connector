f4df8b4650d86f016c4b23572a619807e1627593
Federico Recio <federico.recio@mulesoft.com>
2011-10-05 18:01:52 -0500

    update @module annotation with latest parameter names

--- src/main/java/org/mule/module/mongo/MongoCloudConnector.java
+++ src/main/java/org/mule/module/mongo/MongoCloudConnector.java
@@ -55,7 +55,7 @@ import static org.mule.module.mongo.api.DBObjects.from;
  *
  * @author flbulgarelli
  */
-@Module(name = "mongo", version = "2.0")
+@Module(name = "mongo", schemaVersion = "2.0")
 public class MongoCloudConnector {
 
     private static final String CAPPED_DEFAULT_VALUE = "false";
